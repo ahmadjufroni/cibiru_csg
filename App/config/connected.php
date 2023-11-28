a5ffc78a3108ad24d10516ed1c86c78fb0807af0
Ahmad Jufroni <44368851+ahmadjufroni@users.noreply.github.com>
2023-11-28 14:07:41 +0700

    Update connected.php
    edit file connected

--- App/config/connected.php
+++ App/config/connected.php
@@ -6,10 +6,4 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', '');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-
-
-// define('DB_TYPE', 'mysql');
-// define('DB_HOST', 'localhost');
-// define('DB_NAME', 'ciappsid_db');
-// define('DB_USER', 'ciappsid_ciwa');
-// define('DB_PASS', 'CIWAruga$1000');
+ 
